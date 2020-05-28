f564d294ad662b04839703a182d628206eb4565d
Matt Snyder <matt.snyder@yahoo.com>
2020-05-27 23:47:43 -0400

    first commit

---
+++ BubblWebApp.py
@@ -0,0 +1,24 @@
+from flask import Flask
+import googlemaps
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+gmaps = googlemaps.Client(key='Add Your Key here')
+
+# Geocoding an address
+geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
+
+# Look up an address with reverse geocoding
+reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
+
+# Request directions via public transit
+now = datetime.now()
+directions_result = gmaps.directions("Sydney Town Hall",
+                                     "Parramatta, NSW",
+                                     mode="transit",
+                                     departure_time=now)
